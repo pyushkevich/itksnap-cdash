114813605e635efb368ce9878d86c44455421408
Paul Yushkevich <pyushkevich@gmail.com>
2012-08-27 12:35:48 -0400

    added SETCOND

--- include/macros.cmake
+++ include/macros.cmake
@@ -74,6 +74,45 @@ MACRO(CACHE_ADD Line)
 
 ENDMACRO(CACHE_ADD)
 
+
+# This is a function to set an internal variable in CTest, with the same
+# match pattern as in CACHE_ADD, i.e., 
+#   SETCOND(MyVar MyVal PRODUCT itksnap)
+#
+MACRO(SETCOND Varname Value)
+
+  # Parse out the arguments
+  PARSE_ARGUMENTS(ARG "PRODUCT;CONFIG;BRANCH" "" ${ARGN})
+
+  # Check the matches
+  SET(CACHE_ADD_FLAG TRUE)
+  IF(ARG_PRODUCT)
+    IF(NOT ${IN_PRODUCT} MATCHES ${ARG_PRODUCT})
+      SET(CACHE_ADD_FLAG FALSE)
+    ENDIF(NOT ${IN_PRODUCT} MATCHES ${ARG_PRODUCT})
+  ENDIF(ARG_PRODUCT)
+
+  IF(ARG_BRANCH)
+    IF(NOT ${IN_BRANCH} MATCHES ${ARG_BRANCH})
+      SET(CACHE_ADD_FLAG FALSE)
+    ENDIF(NOT ${IN_BRANCH} MATCHES ${ARG_BRANCH})
+  ENDIF(ARG_BRANCH)
+
+  IF(ARG_CONFIG)
+    IF(NOT ${IN_CONFIG} MATCHES ${ARG_CONFIG})
+      SET(CACHE_ADD_FLAG FALSE)
+    ENDIF(NOT ${IN_CONFIG} MATCHES ${ARG_CONFIG})
+  ENDIF(ARG_CONFIG)
+
+  # Append the value to the INIT_CACHE
+  IF(CACHE_ADD_FLAG)
+    SET(${Varname} ${Value})
+  ENDIF(CACHE_ADD_FLAG)
+
+ENDMACRO(CACHE_ADD)
+
+
+
 # Generate the ENV_ADD function, which can be used by the site-specific
 # scripts to conditionally add variables to the CTEST environment. 
 # The syntax is command is 
