5909b5860044da6c1d9d14eb28f6ea4a98c94139
PICSL builder <picsl@nat-srx550-trust.uphs.upenn.edu>
2014-08-28 17:17:12 -0400

    added centos site

---
+++ sites/pymacmini_centos63/build.cmake
@@ -0,0 +1,53 @@
+#############################################
+# Site-Specific Build Configuration Script  #
+#############################################
+#
+# This script should be edited for each new site where
+# we want to build ITK-SNAP and related tools. The
+# general structure of this script should be followed on
+# all sites, but some additonal variables (e.g., CTEST_ENVIRONMENT)
+# may need to be set on some platforms and configurations
+
+# Library directory: path where all the libraries are built (this is only used internally)
+SET(TKDIR "/home/picsl/tk")
+
+# This is an upload site
+SET(DO_UPLOAD TRUE)
+
+# Set build flags
+SETCOND(CFLAGS "-fno-strict-aliasing -fPIC" CONFIG gcc64rel)
+SETCOND(CFLAGS "-m32 -fno-strict-aliasing -fPIC" CONFIG gcc32rel)
+
+# Allow parallel builds
+ENV_ADD(MAKEFLAGS "-j 2")
+
+# Add cache entries
+CACHE_ADD("MAKECOMMAND:STRING=/usr/bin/make -i -j 2")
+CACHE_ADD("CMAKE_MAKE_PROGRAM:FILEPATH=/usr/bin/make")
+CACHE_ADD("CMAKE_BUILD_TYPE:STRING=Release")
+CACHE_ADD("CMAKE_C_FLAGS:STRING=${CFLAGS}")
+CACHE_ADD("CMAKE_CXX_FLAGS:STRING=${CFLAGS}")
+
+# Tell C3D that the GUI should be built
+CACHE_ADD("BUILD_GUI:BOOLEAN=ON" PRODUCT "c3d")
+
+# We need this because this is a cross-compilation
+CACHE_ADD("CPACK_SYSTEM_NAME:STRING=Linux-i686" CONFIG "gcc32.*")
+
+# Add product-specific cache entries
+IF(NEED_FLTK)
+  SET(SKIP_BUILD ON)
+  # CACHE_ADD("FLTK_BASE_LIBRARY:FILEPATH=${FLTK13}" PRODUCT "itksnap" BRANCH "master")
+ENDIF(NEED_FLTK)
+
+IF(NEED_QT4)
+  SETCOND(QT4DIR "/mnt/build/pauly/Qt/qt-4.8.2-gcc32" CONFIG gcc32rel)
+  SETCOND(QT4DIR "/mnt/build/pauly/Qt/qt-4.8.2" CONFIG gcc64rel)
+  CACHE_ADD("QT_QMAKE_EXECUTABLE:FILEPATH=${QT4DIR}/bin/qmake")
+ENDIF(NEED_QT4)
+
+IF(NEED_QT5)
+  SETCOND(QT5DIR "/home/picsl/Qt/5.3/gcc_64" CONFIG gcc64rel)
+  SETCOND(SKIP_BUILD ON CONFIG gcc32rel)
+  CACHE_ADD("CMAKE_PREFIX_PATH:FILEPATH=${QT5DIR}/lib/cmake" PRODUCT "itksnap" BRANCH "dev32")
+ENDIF(NEED_QT5)
---
+++ sites/pymacmini_centos63/global.cmake
@@ -0,0 +1,6 @@
+# Specify the list of configurations for this site
+SET(CONFIG_LIST "gcc64rel")
+
+# REQUIRED: Where GIT is and who is the user with SSH capability
+SET(GIT_BINARY /usr/bin/git)
+SET(GIT_UID pyushkevich)
