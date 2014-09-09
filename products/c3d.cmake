f78db347dba06541b44f5157abce3ffebcb05f7b
Paul Yushkevich <pyushkevich@gmail.com>
2014-09-09 17:35:03 -0400

    Updated c3d dependencies

--- products/c3d.cmake
+++ products/c3d.cmake
@@ -2,7 +2,7 @@ SET(PRODUCT_CHECKOUT_COMMAND
   "${GIT_BINARY} clone -b ${IN_BRANCH} ssh://${GIT_UID}@git.code.sf.net/p/c3d/git ${IN_PRODUCT}")
 
 # SET UP PRODUCT-SPECIFIC CACHE ENTRIES
-CACHE_ADD("ITK_DIR:PATH=${ROOT}/Nightly/itk/v4.2.1/${IN_CONFIG}")
+CACHE_ADD("ITK_DIR:PATH=${ROOT}/Nightly/itk/v4.5.2/${IN_CONFIG}")
 
 # SPECIFY which products we need
-SETCOND(NEED_QT4 ON BRANCH "master")
+SETCOND(NEED_QT5 ON BRANCH "master")
