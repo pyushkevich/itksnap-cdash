4780a982147d6a8e6aa157e2447aef6b5eff8eb9
Paul Yushkevich <pyushkevich@gmail.com>
2014-09-11 18:45:09 +0100

    Updated build system on WIndows

---
+++ build.bat
@@ -0,0 +1,85 @@
+@echo off
+set MODEL=Nightly
+set PRODUCT_MASK=
+set SKIP_EXTERNAL=
+
+@rem set PATH="%ProgramFiles(x86)%/CMake 2.8/bin;%PATH%"
+@rem set PATH="%ProgramFiles(x86)%/Git/bin;%PATH%"
+@rem 
+
+rem READ OPTIONAL PARAMETERS
+
+
+:loop
+
+IF "%1"=="-e" (
+  set MODEL=Experimental
+  SHIFT
+  GOTO :loop
+)
+
+IF "%1"=="-p" (
+  set PRODUCT_MASK=%2
+  SHIFT
+  SHIFT
+  GOTO :loop
+)
+
+IF "%1"=="-h" (
+  echo "build.bat: ITK-SNAP and friends super-build script"
+  echo "usage:"
+  echo "  build.bat [options] site_name"
+  echo "options:"
+  echo "  -e              Perform experimental build, not nightly"
+  echo "  -p reg_exp      Build products/branches matching regular expression"
+  echo "  -x              Do not build external products (ITK, VTK)"
+  SHIFT
+  GOTO :end
+  GOTO :loop
+)
+
+IF "%1"=="-x" (
+  set SKIP_EXTERNAL=TRUE
+  SHIFT
+  GOTO :loop
+)
+
+set SITE=%1
+set SITESCRIPT="cdash\\sites\\%SITE%\\vars.bat"
+
+rem Check the location
+IF "%SITE%"=="" (
+  echo "Site must be specified as parameter to this script"
+  goto :end
+)
+IF NOT EXIST cdash\build_robot.cmake (
+  echo "You seem to be running this script in the wrong directory"
+  goto :end
+)
+IF NOT EXIST "%SITESCRIPT%" (
+  echo "The script %SITESCRIPT% does not exist for site %SITE%"
+  goto :end
+)
+
+echo "Building model %MODEL% at site %SITE%
+
+CALL "%SITESCRIPT%"
+
+echo Updating the CDASH repo
+pushd cdash
+%GIT_BINARY% pull
+popd
+
+"%CMAKE_BINARY_PATH%/ctest.exe" -V ^
+  -D PRODUCT_MASK:STRING=%PRODUCT_MASK% ^
+  -D SKIP_EXTERNAL:BOOL=%SKIP_EXTERNAL% ^
+  -D GIT_BINARY:STRING="%GIT_BINARY%" ^
+  -D GIT_UID:STRING="%GIT_UID%" ^
+  -D CONFIG_LIST:STRING="%CONFIG_LIST%" ^
+  -D CMAKE_BINARY_PATH:PATH="%CMAKE_BINARY_PATH%" ^
+  -D IN_GLOBAL_MODEL:STRING=%MODEL% ^
+  -D IN_SITE:STRING=%SITE% ^
+  -S cdash\build_robot.cmake
+
+
+:end
--- sites/pymacmini_win7/global.cmake
+++
@@ -1,6 +0,0 @@
-# Specify the list of configurations for this site
-SET(CONFIG_LIST "vce32rel;vce64rel")
-
-# REQUIRED: Where GIT is and who is the user with SSH capability
-SET(GIT_BINARY "C:\\Program Files (x86)\\Git\\bin\\git.exe")
-SET(GIT_UID pyushkevich)
