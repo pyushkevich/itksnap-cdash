fd23724663fa40a524bed8cfe1fb787a9d45a5cb
PICSL Software Build <pyushkevich@gmail.com>
2015-10-05 05:59:52 -0400

    added qt4 build for mac too

--- sites/pymacmini_macos/build.cmake
+++ sites/pymacmini_macos/build.cmake
@@ -11,14 +11,20 @@
 # This site uploads all of its builds
 SET(DO_UPLOAD ON)
 
+# Skip most products when .qt4 options used
+IF(${CONFIG_EXT} MATCHES "qt4")
+  SET(SKIP_BUILD ON)
+  SETCOND(SKIP_BUILD OFF PRODUCT itksnap BRANCH master)
+ENDIF(${CONFIG_EXT} MATCHES "qt4")
+
 # Mac Framework directory
 SET(MINVER 10.7)
 SET(FWDIR "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.7.sdk")
 
 # Depending on the configuration, set the library paths for this machine
 # as well as some other settings
-SETCOND(ARCH i386 CONFIG xc32rel)
-SETCOND(ARCH x86_64 CONFIG xc64rel)
+SETCOND(ARCH i386 CONFIG "xc32.*")
+SETCOND(ARCH x86_64 CONFIG "xc64.*")
 
 ENV_ADD(MAKEFLAGS "-j 8")
 
@@ -49,12 +55,12 @@ IF(NEED_QT4)
 ENDIF(NEED_QT4)
 
 IF(NEED_QT5)
-  SETCOND(SKIP_BUILD ON CONFIG xc32rel)
+  SETCOND(SKIP_BUILD ON CONFIG "xc32.*")
   CACHE_ADD("CMAKE_PREFIX_PATH:STRING=${TKDIR}/Qt5/5.3/clang_64/lib/cmake")
 ENDIF(NEED_QT5)
   
 IF(NEED_QT54)
-  SETCOND(SKIP_BUILD ON CONFIG xc32rel)
+  SETCOND(SKIP_BUILD ON CONFIG "xc32.*")
   CACHE_ADD("CMAKE_PREFIX_PATH:STRING=${TKDIR}/Qt5.4/5.4/clang_64/lib/cmake")
 ENDIF(NEED_QT54)
   
--- sites/pymacmini_macos/vars.sh
+++ sites/pymacmini_macos/vars.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-CONFIG_LIST="xc64rel;xc32rel"
+CONFIG_LIST="xc64rel;xc64rel.qt4"
 GIT_BINARY=/usr/bin/git
 GIT_UID=pyushkevich
 CMAKE_BINARY_PATH=/usr/bin
