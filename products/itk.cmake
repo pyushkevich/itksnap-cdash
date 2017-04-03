9a426bc139fdcf0fe9930eff6e22da93b0ecd88e
PICSL builder <picsl@localhost.localdomain>
2017-04-03 08:40:20 -0400

    Linux changes, ITK url to https

--- products/itk.cmake
+++ products/itk.cmake
@@ -1,10 +1,10 @@
 # In order to retrieve a tag, we must string together two git commands, which requires calling the system shell
 IF(UNIX)
   SET(PRODUCT_CHECKOUT_COMMAND 
-    "sh -c \"${GIT_BINARY} clone http://itk.org/ITK.git ${IN_PRODUCT} && cd ${IN_PRODUCT} && ${GIT_BINARY} checkout ${IN_BRANCH}\"")
+    "sh -c \"${GIT_BINARY} clone https://itk.org/ITK.git ${IN_PRODUCT} && cd ${IN_PRODUCT} && ${GIT_BINARY} checkout ${IN_BRANCH}\"")
 ELSEIF(WIN32)
   SET(PRODUCT_CHECKOUT_COMMAND 
-      "\"${GIT_BINARY}\" clone -b ${IN_BRANCH} http://itk.org/ITK.git ${IN_PRODUCT}")
+      "\"${GIT_BINARY}\" clone -b ${IN_BRANCH} https://itk.org/ITK.git ${IN_PRODUCT}")
 ENDIF(UNIX)
 
 SET(PRODUCT_EXTERNAL ON)
