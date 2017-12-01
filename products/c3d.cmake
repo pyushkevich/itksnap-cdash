af6426cc755a281d29163565425c10eeafabe281
Paul Yushkevich <pyushkevich@gmail.com>
2017-12-01 14:00:06 -0500

    upped ITK version for c3d

--- products/c3d.cmake
+++ products/c3d.cmake
@@ -2,7 +2,7 @@ SET(PRODUCT_CHECKOUT_COMMAND
   "${GIT_BINARY} clone -b ${IN_BRANCH} ssh://${GIT_UID}@git.code.sf.net/p/c3d/git ${IN_PRODUCT}")
 
 # SET UP PRODUCT-SPECIFIC CACHE ENTRIES
-CACHE_ADD("ITK_DIR:PATH=${ROOT}/Nightly/itk/v4.5.2/${IN_CONFIG}")
+CACHE_ADD("ITK_DIR:PATH=${ROOT}/Nightly/itk/v4.12.2/${IN_CONFIG}")
 
 # SPECIFY which products we need
 SETCOND(NEED_QT54 ON BRANCH "master")
