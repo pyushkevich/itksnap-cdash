ced0d8eb864895f28a15c2eb41b7d8b6c8e6b308
Paul Yushkevich <pyushkevich@gmail.com>
2017-12-01 07:08:07 -0500

    set up picslbuild to actually build stuff

--- sites/picslbuild/vars.sh
+++ sites/picslbuild/vars.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-CONFIG_LIST="icc64rel;gcc64rel;gcc64dbg"
+CONFIG_LIST="gcc64rel.qt4;gcc64dbg;gcc64rel"
 GIT_BINARY=/usr/bin/git
 GIT_UID=pyushkevich
 CMAKE_BINARY_PATH=/mnt/build/pauly/cmake/cmake-3.3.0-Linux-x86_64/bin
