d8330bcc807b68ec2dd85c5e3fe2c39e0e29d24d
Paul Yushkevich <pyushkevich@gmail.com>
2021-04-21 20:32:48 +0000

    bumped cmrep ITK version

--- products/cmrep.cmake
+++ products/cmrep.cmake
@@ -2,5 +2,5 @@ SET(PRODUCT_CHECKOUT_COMMAND
   "${GIT_BINARY} clone -b ${IN_BRANCH} https://github.com/pyushkevich/cmrep.git ${IN_PRODUCT}")
 
 # SET UP PRODUCT-SPECIFIC CACHE ENTRIES
-CACHE_ADD("ITK_DIR:PATH=${ROOT}/Nightly/itk/v4.13.2/${IN_CONFIG}" BRANCH "master")
+CACHE_ADD("ITK_DIR:PATH=${ROOT}/Nightly/itk/v5.1.2/${IN_CONFIG}" BRANCH "master")
 CACHE_ADD("VTK_DIR:PATH=${ROOT}/Nightly/vtk/v6.3.0/${IN_CONFIG}" BRANCH "master")
