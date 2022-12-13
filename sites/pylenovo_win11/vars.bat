82b6d795d54ab6fa53da755aa123ffcad06cd277
Paul Yushkevich <pyushkevich@gmail.com>
2022-12-13 11:21:16 -0800

    Added new lenovo build machine

---
+++ sites/pylenovo_win11/vars.bat
@@ -0,0 +1,5 @@
+@set HOME=C:\Users\pyush
+@set GIT_BINARY=%ProgramFiles%\Git\bin\git.exe
+@set CMAKE_BINARY_PATH=%ProgramFiles%\CMake\bin
+@set GIT_UID=pyushkevich
+@set CONFIG_LIST="vce64rel;vce64dbg"
